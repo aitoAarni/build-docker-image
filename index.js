744b80fb17d4f8dd94827c195307a410bd8fbcc9
aitoAarni <aarni.rechardt@gmail.com>
2025-04-05 01:51:48 +0300

    change2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello to the globes</h1>")
+    res.send("<h1>New world order!</h1>")
 })
 
 const PORT = 8080
